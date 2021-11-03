15394ab51f17045cd799b1c7c92bf94286edc6ae
Seo Shin Youn <89275498+SeoShinYoun@users.noreply.github.com>
2021-11-03 18:55:07 +0800

    Completion of onboarding screen describing the 3 main benefits of our project, allows user to skip, hit next, and get started.

---
+++ app/src/main/java/com/example/hawkerpals/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.example.hawkerpals
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var onboardingItemsAdapter: OnboardingItemsAdapter
+    private lateinit var indicatorsContainer: LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setOnboardingItems()
+        setupIndicators()
+        setCurrentIndicators(0)
+    }
+
+    private fun setOnboardingItems(){
+        onboardingItemsAdapter = OnboardingItemsAdapter(
+            listOf(
+                OnboardingItem(
+                    onboardingImage = R.drawable.task,
+                    title = "Connect with other hawker stalls",
+                    description = "Publicise & promote your dishes to other businesses while getting to know other hawkers better"
+                ),
+                OnboardingItem(
+                    onboardingImage = R.drawable.reminder,
+                    title = "Diversify your income stream with B2B Sales",
+                    description = "Buy & sell soon-to-be expired ingredients from fellow hawkers"
+                ),
+                OnboardingItem(
+                    onboardingImage = R.drawable.time,
+                    title = "Reduce retail food waste",
+                    description = "Cut your losses and reduce your cost for low demand food ingredients"
+                )
+            )
+        )
+        val onboardingViewPager = findViewById<ViewPager2>(R.id.onboardingViewPager)
+        onboardingViewPager.adapter = onboardingItemsAdapter
+        onboardingViewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicators(position)
+            }
+        })
+        (onboardingViewPager.getChildAt(0)as RecyclerView).overScrollMode =
+            RecyclerView.OVER_SCROLL_NEVER
+        findViewById<ImageView>(R.id.imageNext).setOnClickListener{
+            if (onboardingViewPager.currentItem + 1 < onboardingItemsAdapter.itemCount){
+                onboardingViewPager.currentItem += 1
+            }else{
+                navigateToHomeActivity()
+            }
+        }
+        findViewById<TextView>(R.id.textSkip).setOnClickListener{
+            navigateToHomeActivity()
+        }
+        findViewById<MaterialButton>(R.id.buttonGetStarted).setOnClickListener{
+            navigateToHomeActivity()
+        }
+    }
+    private fun navigateToHomeActivity(){
+        startActivity(Intent(applicationContext, HomeActivity::class.java))
+        finish()
+    }
+
+    private fun setupIndicators(){
+        indicatorsContainer = findViewById(R.id.indicatorsContainer)
+        val indicators = arrayOfNulls<ImageView>(onboardingItemsAdapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(8, 0, 8, 0)
+        for (i in indicators.indices){
+            indicators[i] = ImageView(applicationContext)
+            indicators[i]?.let{
+                it.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+                it.layoutParams = layoutParams
+                indicatorsContainer.addView(it)
+            }
+        }
+    }
+
+    private fun setCurrentIndicators(position: Int){
+        val childCount = indicatorsContainer.childCount
+        for (i in 0 until childCount){
+            val imageView = indicatorsContainer.getChildAt(i) as ImageView
+            if (i == position){
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active_background
+                    )
+                )
+            }
+            else{
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hawkerpals/OnboardingItem.kt
@@ -0,0 +1,7 @@
+package com.example.hawkerpals
+
+data class OnboardingItem(
+    val onboardingImage: Int,
+    val title: String,
+    val description: String
+)
