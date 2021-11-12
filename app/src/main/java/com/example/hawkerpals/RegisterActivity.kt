4d493301894dce31bb8a1e9645d4e66a87fc0702
Ho Shang Luen Owen <s10205346@connect.np.edu.sg>
2021-11-12 13:39:49 +0800

    Updated data class based on Kai Jun's fields, added unique IDs assigned to users during registration

--- app/src/main/java/com/example/hawkerpals/RegisterActivity.kt
+++ app/src/main/java/com/example/hawkerpals/RegisterActivity.kt
@@ -4,14 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
@@ -59,11 +60,29 @@ class RegisterActivity : AppCompatActivity() {
 
                                     //[START of writing to Firebase Realtime Database]
                                     val db = Firebase.database("https://hawkerpals-de16f-default-rtdb.asia-southeast1.firebasedatabase.app/")
-                                    val dbRef = db.getReference("users")
-                                    val user = User(fullName = null,stallAddress = null, email = null,userType = null)
-                                    user.email = email
-                                    //After adding more input boxes(eg full name, userType, etc) to the registration screen, data can be assigned right after this line
-                                    dbRef.child(email.replace(".","DOT")).setValue(user)
+                                    val dbRef = db.getReference("user_info")
+                                    val user = User(user_email = null, user_id = null, user_name = null, user_type = null)
+                                    val availableIDs: MutableList<Int> = mutableListOf()
+                                    var userKey = "user_"
+                                    for(x in (1..9999)){
+                                        availableIDs.add(x)
+                                    }
+
+                                    dbRef.get().addOnSuccessListener {
+                                        for (users in it.children){
+                                            for (data in users.children){
+                                                if(data.key == "user_id"){
+                                                    availableIDs.remove(data.value.toString().toInt())
+                                                }
+                                            }
+                                        }
+                                        user.user_email = email //After adding more input boxes(eg full name, userType, etc) to the registration screen, data can be assigned right after this line
+                                        user.user_id = turnIntTo4Char(availableIDs.elementAt(0))
+                                        userKey += user.user_id
+                                        dbRef.child(userKey).setValue(user)
+                                    }.addOnFailureListener{
+                                        Log.e("firebase", "Error getting data", it)
+                                    }
                                     //[END of writing to Firebase Realtime Database]
 
                                     Toast.makeText(
@@ -92,5 +111,20 @@ class RegisterActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+    private fun turnIntTo4Char(id: Int): String {
+        if (id>999){
+            return id.toString()
+        }
+        if (id>99){
+            return ("0"+id)
+        }
+        if (id>9){
+            return ("00"+id)
+        }
+        if (id>0){
+            return ("000"+id)
+        }
+        return "NO ID"
     }
 }
