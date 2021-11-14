f2771942424b6d1c0699415bff293567800343a5
LuoYuXuan <reathan@126.com>
2021-11-14 15:16:26 +0800

    update profile fragment

--- app/src/main/java/com/example/hawkerpals/bottomnavbartutorial/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/hawkerpals/bottomnavbartutorial/fragments/ProfileFragment.kt
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
+import com.example.hawkerpals.HomeActivity
 import com.example.hawkerpals.LoginActivity
 import com.example.hawkerpals.R
 import com.google.android.gms.tasks.OnCompleteListener
@@ -22,63 +24,68 @@ import kotlinx.android.synthetic.main.fragment_profile.*
 
 
 class ProfileFragment : Fragment() {
-    var TAG = "ActivityLifeCycle"
 
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        Log.d(TAG,"===>fragment===>onAttach()")
+
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG,"===>fragment===>onCreate()")
-        logout.setOnClickListener{
-            FirebaseAuth.getInstance().signOut()
-            val intent = Intent (getActivity(), LoginActivity::class.java)
-            getActivity()?.startActivity(intent)
-        }
-    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        Log.d(TAG, "===>fragment===>onCreateView()")
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val v = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val btn = v.findViewById<Button>(R.id.logout)
+        btn.setOnClickListener{
+            val intent = Intent (getActivity(), LoginActivity::class.java)
+            getActivity()?.startActivity(intent)
+            FirebaseAuth.getInstance().signOut()
+        }
+
+        return v
+
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(TAG, "===>fragment===>onStart()")
+
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(TAG, "===>fragment===>onResume()")
+
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(TAG, "===>fragment===>onPause()")
+
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(TAG, "===>fragment===>onStop()")
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        Log.d(TAG, "===>fragment===>onDestroyView()")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG, "===>fragment===>onDestroy()")
     }
 
     override fun onDetach() {
         super.onDetach()
-        Log.d(TAG, "===>fragment===>onDetach()")
     }
 
 
