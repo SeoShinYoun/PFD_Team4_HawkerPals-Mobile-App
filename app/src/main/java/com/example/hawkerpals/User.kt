63e1787969311d20abad80be85f797917a3e6b9a
Ho Shang Luen Owen <s10205346@connect.np.edu.sg>
2021-11-17 23:01:10 +0800

    added more userType and vaccination input to registration page

--- app/src/main/java/com/example/hawkerpals/User.kt
+++ app/src/main/java/com/example/hawkerpals/User.kt
@@ -3,7 +3,8 @@ package com.example.hawkerpals
 data class User(var user_email : String? = null,
                 var user_id : String? = null,
                 var user_name : String? = null,
-                val user_type : String? = null){
+                var user_type : String? = null,
+                var vacinated: Boolean? = null){
 
 
 }
