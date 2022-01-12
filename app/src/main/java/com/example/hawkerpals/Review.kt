70d169b1664b8f62e7af0ff68d98f26ccbe47041
Kai Jun Fun <s10194271@connect.np.edu.sg>
2022-01-12 12:15:46 +0800

    adding review data class

---
+++ app/src/main/java/com/example/hawkerpals/Review.kt
@@ -0,0 +1,7 @@
+package com.example.hawkerpals
+
+data class Review(var review_id: String? = null,
+                  var review_title: String? = null,
+                  var review_description: String? = null,
+                  var groupname: String? = null
+)
